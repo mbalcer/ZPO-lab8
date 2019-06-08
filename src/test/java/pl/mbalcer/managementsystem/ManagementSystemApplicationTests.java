25fc8202afe2895582e14acbe19f7c26f4441a9a
mbalcer <mateusz75319@gmail.com>
2019-06-08 12:29:26 +0200

    Initial commit

---
+++ src/test/java/pl/mbalcer/managementsystem/ManagementSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mbalcer.managementsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagementSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
