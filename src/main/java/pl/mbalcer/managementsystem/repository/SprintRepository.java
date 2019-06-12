d11375edfc52cbe0fbb3eca4946d865bc5584d9b
mbalcer <mateusz75319@gmail.com>
2019-06-12 19:42:23 +0200

    Created sprint service

--- src/main/java/pl/mbalcer/managementsystem/repository/SprintRepository.java
+++ src/main/java/pl/mbalcer/managementsystem/repository/SprintRepository.java
@@ -1,7 +1,12 @@
 package pl.mbalcer.managementsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.mbalcer.managementsystem.model.entity.Project;
 import pl.mbalcer.managementsystem.model.entity.Sprint;
 
+import java.util.List;
+
 public interface SprintRepository extends JpaRepository<Sprint, Long> {
+    List<Sprint> findAllByProject(Project project);
+
 }
---
+++ src/main/java/pl/mbalcer/managementsystem/service/SprintService.java
@@ -0,0 +1,38 @@
+package pl.mbalcer.managementsystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.mbalcer.managementsystem.model.entity.Project;
+import pl.mbalcer.managementsystem.model.entity.Sprint;
+import pl.mbalcer.managementsystem.model.entity.User;
+import pl.mbalcer.managementsystem.repository.SprintRepository;
+
+import java.util.List;
+
+@Service
+public class SprintService {
+
+    @Autowired
+    private SprintRepository sprintRepository;
+
+    public Sprint getSprint(Long id) {
+        return sprintRepository.getOne(id);
+    }
+
+    public List<Sprint> getAllSprintByProject(Project project) {
+        return sprintRepository.findAllByProject(project);
+    }
+
+    public Sprint createSprint(Sprint user) {
+        return sprintRepository.save(user);
+    }
+
+    public Sprint updateSprint(Sprint user) {
+        return sprintRepository.save(user);
+    }
+
+    public void deleteSprint(Long id) {
+        sprintRepository.deleteById(id);
+    }
+
+}
