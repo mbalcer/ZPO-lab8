4be2fd4f0970826d0b3102f51165d9c3425ed896
mbalcer <mateusz75319@gmail.com>
2019-06-13 17:38:32 +0200

    Created a table with sprint list

--- src/main/java/pl/mbalcer/managementsystem/ui/SprintPanel.java
+++ src/main/java/pl/mbalcer/managementsystem/ui/SprintPanel.java
@@ -1,10 +1,15 @@
 package pl.mbalcer.managementsystem.ui;
 
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import pl.mbalcer.managementsystem.model.entity.Project;
+import pl.mbalcer.managementsystem.model.entity.Sprint;
 import pl.mbalcer.managementsystem.model.entity.User;
 import pl.mbalcer.managementsystem.service.AllService;
+
+import java.util.List;
 
 public class SprintPanel {
     private LoginPanel loginPanel;
@@ -32,10 +37,26 @@ public class SprintPanel {
     public VerticalLayout getLayout() {
         sprintLayout = new VerticalLayout();
 
-        Label show = new Label("abc");
-        
-        sprintLayout.addComponent(show);
+        Label titleLabel = new Label();
+        titleLabel.setValue("List of sprints for '"+project.getName()+"' project");
 
+        Grid<Sprint> sprintGrid = initSprintTable();
+
+        sprintLayout.addComponents(titleLabel, sprintGrid);
+        sprintLayout.setComponentAlignment(titleLabel, Alignment.TOP_CENTER);
+        sprintLayout.setComponentAlignment(sprintGrid, Alignment.MIDDLE_CENTER);
         return sprintLayout;
+    }
+
+    private Grid<Sprint> initSprintTable() {
+        Grid<Sprint> sprintGrid = new Grid<>();
+        List<Sprint> sprintList = allService.getSprintService().getAllSprintByProject(project);
+        sprintGrid.setItems(sprintList);
+        sprintGrid.addColumn(Sprint::getId).setCaption("##");
+        sprintGrid.addColumn(Sprint::getDateFrom).setCaption("Date from");
+        sprintGrid.addColumn(Sprint::getDateTo).setCaption("Date to");
+        sprintGrid.addColumn(Sprint::getPlannedStoryPoints).setCaption("Max story points");
+
+        return sprintGrid;
     }
 }
